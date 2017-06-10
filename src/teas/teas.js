b7bc30767d8fa2ebf3252046cc7b8202c1618e51
Jimmy Douieb <jimmydouieb@gmail.com>
2017-06-10 18:53:10 +0300

    Home assignment

--- src/teas/teas.js
+++ src/teas/teas.js
@@ -2,8 +2,15 @@ import angular from 'angular';
 import { MODULE_NAME } from '../config'
 
 const module = angular.module(MODULE_NAME)
-    .controller('TeasCtrl', function () {
+    .controller('TeasCtrl', function (Restangular) {
 
+        var teas = Restangular.all('teas');
+
+        teas.getList().then((teas) => {
+            this.teas = teas;
+        });
+
+        this.orderProp = 'name';
     })
 
 module.config(function ($routeProvider) {
